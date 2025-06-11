2f0a2e17398a5a5dbe4ff5ba63646be6d0ec4c9b
sahanalab <sahanaa.scs24@bmsce.ac.in>
2025-06-11 12:42:12 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,5 +1,5 @@
 public class test {
   public static void main(String[] args) {
-    System.out.println("Hello from Jenkins Java Build!");
+    System.out.println("Hello from changed Jenkins Java Build!");
   }
 }
